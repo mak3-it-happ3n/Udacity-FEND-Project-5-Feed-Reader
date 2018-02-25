2e23aadedc45495c426a71ca7369bf694be09cac
Christoph Pätzold <ogd998@gmail.com>
2018-02-25 17:53:44 +0100

    hidden menu done

--- js/app.js
+++ js/app.js
@@ -8,7 +8,6 @@
 
 // The names and URLs to all of the feeds we'd like available.
 var allFeeds = [
-
     {
         name:'Udacity Blog',
         url: 'http://blog.udacity.com/feed'
@@ -22,7 +21,6 @@ var allFeeds = [
         name: 'Linear Digressions',
         url: 'http://feeds.feedburner.com/udacity-linear-digressions'
     }
-
 ];
 
 /* This function starts up our application. The Google Feed
